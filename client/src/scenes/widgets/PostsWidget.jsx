fed5609af1a48530bf37830a7199b37e10d78dfc
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-04-16 20:48:46 +0700

    post and list posts

---
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setPosts } from 'state';
+import PostWidget from './PostWidget';
+
+const PostsWidget = ({ userId, isProfile = false }) => {
+  return <div>PostsWidget</div>;
+};
+
+export default PostsWidget;
